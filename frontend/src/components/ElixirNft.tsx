c8ed34b4958c7bf914c526b7f958ff3c6fc139be
Viet <vionjs@gmail.com>
2022-05-08 22:05:25 +0700

    Finish get list of nft

--- frontend/src/components/ElixirNft.tsx
+++ frontend/src/components/ElixirNft.tsx
@@ -1,11 +1,14 @@
 import { Button } from "@mui/material";
 
 const ElixirNft = (props: {
-  imageUri: string
+  imageUri: string,
+  permalink: string
 }) => {
   return (
     <div className="ElixirNftContainer">
-      <img className="ElixirNftImage" src={props.imageUri} alt="" />
+      <img className="ElixirNftImage" src={props.imageUri} alt="" onClick={() => {
+        window.open(props.permalink, '_blank')
+      }} />
       <div className="ElixirNftActions">
         <Button color="secondary" variant="contained">Merge</Button>
       </div>
--- frontend/src/components/Merge.tsx
+++ frontend/src/components/Merge.tsx
@@ -4,6 +4,8 @@ import ElixirNft from "./ElixirNft";
 import config from '../config/config.json';
 import { IOpenseaAsset, OpenseaResponse } from "../models/Ethereum";
 import { getContractAddressByName } from "../helpers/Contract";
+import { Button } from "@mui/material";
+import RefreshIcon from '@mui/icons-material/Refresh';
 
 const Merge = () => {
   const { account, active, activate, chainId, library } = useWeb3React();
@@ -12,13 +14,12 @@ const Merge = () => {
 
   useEffect(() => {
     if (account) {
-      fetchData(`${config.OPENSEA_API_URL}/assets?owner=${account}`);
+      fetchData();
     }
-    // eslint-disable-next-line
   }, [account]);
 
-  const fetchData = (url: string) => {
-    return fetch(url)
+  const fetchData = () => {
+    return fetch(`${config.OPENSEA_API_URL}/assets?owner=${account}`)
       .then((response) => response.json())
       .then((data: OpenseaResponse) => {
         const nfts = data.assets.filter(
@@ -30,9 +31,21 @@ const Merge = () => {
 
   return (
     <div className="MergeContainer">
-      <div className="ElixirNftsContainer">
-        {elixirNfts.map((elixirNft: IOpenseaAsset) => <ElixirNft key={elixirNft.id} imageUri={elixirNft.image_preview_url} />)}
+      <div className="MergeTitleContainer">
+        <span className="MergeTitle">Elixir Nfts</span>
       </div>
+      <div className="MergeActions">
+        <Button color="secondary" variant="contained" onClick={fetchData}>
+          <RefreshIcon />
+          Refresh
+        </Button>
+      </div>
+      {elixirNfts.length > 0 ?
+      <div className="ElixirNftsContainer">
+        {elixirNfts.map((elixirNft: IOpenseaAsset) => <ElixirNft key={elixirNft.id} imageUri={elixirNft.image_preview_url} permalink={elixirNft.permalink} />)}
+      </div> :
+      <span className="NoElixirNftMessage">You have no Elixir NFT yet.</span>
+      }
     </div>
   );
 }
